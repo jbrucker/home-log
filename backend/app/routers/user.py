2126a0eb35891648f2a2b1218070019e7b86f5b3
jbrucker <jebrucker@gmail.com>
2025-06-24 18:02:41 +0700

    first try at a router

--- backend/app/routers/user.py
+++ backend/app/routers/user.py
@@ -6,16 +6,25 @@
 from http import HTTPStatus
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.core.database import get_db
-from app import schemas
+from app.core.database import db
+from app import models, schemas
 from app.data_access import user_dao
 
-router = APIRouter(prefix="/users", tags=["users"])
+router = APIRouter(tags=["users"])  # can use prefix="/users" to factor out path prefix.
 
-@router.post("/", status_code=HTTPStatus.CREATED, response_model=schemas.User)
-async def register_user(user_data: schemas.UserCreate, session: AsyncSession = Depends(get_db)):
-    existing_user = await user_dao.get_user_by_email(db, user_in.email)
+@router.get("/users/{user_id}", response_model=schemas.User)
+async def get_user(user_id: int, session: AsyncSession = Depends(db.get_session)):
+    user = await user_dao.get_user_by_id(session, user_id)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user  # FastAPI will use from_attributes=True to convert to schema
+
+
+@router.post("/users", status_code=HTTPStatus.CREATED, response_model=schemas.User)
+async def register_user(user_data: schemas.UserCreate, 
+                        session: AsyncSession = Depends(db.get_session)):
+    existing_user = await user_dao.get_user_by_email(session, user_data.email)
     if existing_user:
         raise HTTPException(status_code=400, detail="Email already registered")
-    return await user_dao.create_user(db, user_data)
+    return await user_dao.create_user(session, user_data)
 
