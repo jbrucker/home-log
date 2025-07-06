53c5189fe0a4a04fbaa4b10c2b240bb816eabda8
jbrucker <jebrucker@gmail.com>
2025-07-06 12:24:31 +0700

    Remove id field from UserPassword, use  as both primary and foreign key

--- backend/app/models.py
+++ backend/app/models.py
@@ -3,6 +3,21 @@
    Passwords are used only for local authentication, so
    not all users have a password. Hence a separate table.
    Users may be authenticated by other means, e.g. OAuth.
+
+   We need client-side computation of the current datetime 
+   (and timezone-aware) for two reasons:
+ 
+   1. Testing with SQLite.  SQLite does not provide timezone-aware timestamps. 
+      func.now() returns a naive UTC value, even if "TIMESTAMP(timezone=True)"
+      is used in the model.
+      Unfortunately, client-side computation of datetime doesn't fix this.
+      SQLite *always* returns timezone unaware datetimes.
+  
+   2. We want user.updated_at to be accessible outside of a Session. 
+      For async access, if updated_at is defined with "onupdate=func.now" 
+      then an attempt is made to access the server, which raises a 
+      MissingGreenlet error when done outside the scope of a session.
+
 """
 
 # DateTime, Integer, TIMESTAMP are convenience classes for sqlalchemy.sql.sqltypes.{name}
@@ -18,15 +33,6 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.core.database import Base, db
 
 
-# We need client-side computation of current datetime (and timezone-aware) for two reasons:
-#
-# 1. Testing with SQLite.  SQLite does not provide timezone-aware timestamps. 
-#    func.now() returns a naive UTC value, even if "TIMESTAMP(timezone=True)" is used in the model.
-# 2. We want user.updated_at to be accessible outside of a Session. For async access, 
-#    if updated_at is defined with "onupdate=func.now" then an attempt is made to access
-#    the server, which causes a MissingGreenlet error when done outside the scope of a session.
-#
-# Possible solution: do the current datetime computation entirely on the client side.
 
 def utcnow() -> datetime:
     """Return the current datetime as a timezone aware value."""
@@ -57,29 +63,32 @@ class User(Base):
                                     )
 
     # a uni-directional relationship.  
-    # To make it bidirectional use backref=... or define the relationship in both classes & use back_populates
+    # To make it bidirectional use backref=... 
+    # use use back_populates=... on BOTH sides of the relationship
     user_password: Mapped["UserPassword"] = relationship("UserPassword", 
-                                                # backref=("user",uselist=False),
-                                                uselist=False,  
-                                                cascade="all, delete-orphan"
-                                            )
+                                         # backref=("user",uselist=False),
+                                         uselist=False,  
+                                         cascade="all, delete-orphan"
+                                         )
 
 
 class UserPassword(Base):
-    """Store a user's password as a hash. Only for locally authenticated users."""
+    """Store user's password as a hash. Only for locally authenticated users."""
     __tablename__ = "user_passwords"
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
+    user_id: Mapped[int] = mapped_column(Integer,
+                                         ForeignKey("users.id", ondelete="CASCADE"),
+                                         primary_key=True, 
+                                         nullable=False)
     hashed_password: Mapped[str] = mapped_column(String, nullable=False)
     updated_at: Mapped[datetime] = mapped_column(
                                         TIMESTAMP(timezone=True), 
                                         # server_default=func.now()
                                         default=utcnow
                                     )
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id", 
-                                        ondelete="CASCADE"),
-                                        nullable=False)
     
 
-def initialize_schema(engine):
-    """Create the table schema."""
-    db.create_tables()
+# For testing. Do this in app/core/database.py 
+#
+#def initialize_schema(engine):
+#    """Create the table schema."""
+#    db.create_tables()
